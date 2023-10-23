8237be9250d28a7a382c5f86939e4dfdafbb5928
rinnnn55 <125744070+rinnnn55@users.noreply.github.com>
2023-10-23 22:27:51 +0900

    rh6

--- src/App.jsx
+++ src/App.jsx
@@ -5,70 +5,14 @@ export default function App() {
     const random = Math.floor(Math.random()*array.length);
     return array[random];
   }
+
   const [xItem, setXItem] = useState("");
   const [yItem, setYItem] = useState("");
   const [zItem, setZItem] = useState("");
   const [showStory, setShowStory] = useState(false);
-  
-  
-  function generateStory() {
-    setXItem(randomValueFromArray(['Willy the Goblin', 'Big Daddy', 'Father Christmas']));
-    setYItem(randomValueFromArray(['the soup kitchen', 'Disneyland', 'the White House']));
-    setZItem(randomValueFromArray(['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']));
-    setShowStory(true);
-  }
-  return (
-    <>
-      <div>
-        <label htmlFor="customname">Enter custom name:</label>
-        <input type="text" placeholder="" />
-      </div>
-      <div>
-        <label htmlFor="us">US</label>
-        <input type="radio" value="us" checked={ukus === "us"} />
-        <label htmlFor="uk">UK</label>
-        <input type="radio" value="uk" checked={ukus === "uk"} />
-      </div>
-      <div>
-        <button onClick={generateStory}>Generate random story</button>
-      </div>
-      {showStory && (
-        <p>
-          It was 94 fahrenheit outside, so {xItem} went for a walk. When they
-          got to {yItem}, they stared in horror for a few moments, then {zItem}.
-          Bob saw the whole thing, but was not surprised — {xItem} weighs 300
-          pounds, and it was a hot day.
-        </p>
-      )}
-    </>
-  );
+  const [name, setName] = useState("");
+  const [ukus, setUkus] = useState("us");
 
-}
-
-
-
-
-
-
-
-
-/*
-import { useState } from "react";
-
-export default function App() {
-  // ランダムな値を配列から取得する関数
-  function randomValueFromArray(array) {
-    const random = Math.floor(Math.random()*array.length);
-    return array[random];
-  }
-
-  // ストーリーの要素と表示状態を管理するstate
-  const [xItem, setXItem] = useState("");
-  const [yItem, setYItem] = useState("");
-  const [zItem, setZItem] = useState("");
-  const [showStory, setShowStory] = useState(false);
-
-  // ランダムなストーリーを生成する関数
   function generateStory() {
     setXItem(randomValueFromArray(['Willy the Goblin', 'Big Daddy', 'Father Christmas']));
     setYItem(randomValueFromArray(['the soup kitchen', 'Disneyland', 'the White House']));
@@ -80,30 +24,24 @@ export default function App() {
     <>
       <div>
         <label htmlFor="customname">Enter custom name:</label>
-        <input type="text" placeholder="" />
+        <input type="text" placeholder="" onChange={d => setName(d.target.value)}/>
       </div>
       <div>
         <label htmlFor="us">US</label>
-        <input type="radio" value="us" />
+        <input type="radio" value="us" checked={ukus === "us"} onChange={e => setUkus(e.target.value)} />
         <label htmlFor="uk">UK</label>
-        <input type="radio" value="uk" />
+        <input type="radio" value="uk" checked={ukus === "uk"} onChange={e => setUkus(e.target.value)} />
       </div>
       <div>
         <button onClick={generateStory}>Generate random story</button>
       </div>
       {showStory && (
         <p>
-          It was 94 fahrenheit outside, so {xItem} went for a walk. When they
+          It was {ukus === "us" ? "94 fahrenheit" : "34.4 degrees Celsius"} outside, so {xItem} went for a walk. When they
           got to {yItem}, they stared in horror for a few moments, then {zItem}.
-          Bob saw the whole thing, but was not surprised — {xItem} weighs 300
-          pounds, and it was a hot day.
+          {name ? name : "Bob"} saw the whole thing, but was not surprised — {xItem} weighs {ukus === "us" ? "300 pounds" : "21.4 stones"}, and it was a hot day.
         </p>
       )}
     </>
   );
 }
-
-
-*/
-
-
